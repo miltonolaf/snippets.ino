a6408ab941b79cd98bd8076c2da6428041088ce4
Milton Olaf Paredes <miltonolaf@outlook.com>
2018-10-18 20:10:02 -0500

    fix defs and prints

--- mini-light-control/mini-light-control.ino
+++ mini-light-control/mini-light-control.ino
@@ -1,24 +1,29 @@
-float input = A0;
-float output = A2;
-float data;
-float value;
-float threshold; 
+float in_ph = A0;
+float in_th = A2;
+float photo_r, value, threshold; 
 
 void setup() {
-  Serial.begin(9600); //inicia la comunicación serial a un baud rate de 9600
+  Serial.begin(9600);
 }
 
 void loop() {
-  data = analogRead(input);
-  threshold = analogRead(output);
-  value = ((data * 5)/1023);
-  Serial.println(data); //pinrtln = salto de linea
-  Serial.println(threshold); //pinrtln = salto de linea
-  if(data > threshold){
-    analogWrite(12, data/4);
+  photo_r = analogRead(in_ph);
+  threshold = analogRead(in_th);
+  value = ((photo_r * 5)/1023);
+  
+  Serial.print("Reading = ");
+  Serial.print("\t");
+  Serial.print(value);
+  Serial.print("\t\t");
+  Serial.print("Threshold = ");
+  Serial.print("\t");
+  Serial.println(threshold);
+  
+  if(photo_r > threshold){
+    analogWrite(12, photo_r/4);
   }else{
     analogWrite(12, LOW);
   }
-  delay(500);
   
+  delay(500);
 }
